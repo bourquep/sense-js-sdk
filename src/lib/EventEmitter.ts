55b240f318b1288a8126424ee60c973c30f4db27
Pascal Bourque <pascal@cosmos.moi>
2025-04-20 14:58:24 -0400

    fix: Make EventEmitter conform to NodeJS.EventEmitter (#9)

--- src/lib/EventEmitter.ts
+++ src/lib/EventEmitter.ts
@@ -30,11 +30,11 @@ import { EventEmitter as NodeEventEmitter } from 'node:events';
  * Source: {@link https://blog.makerx.com.au/a-type-safe-event-emitter-in-node-js}
  */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-export class EventEmitter<TEvents extends Record<string, any>> {
+export class EventEmitter<TEvents extends Record<string, any>> implements NodeJS.EventEmitter {
   private _emitter = new NodeEventEmitter();
 
   emit<TEventName extends keyof TEvents & string>(eventName: TEventName, ...eventArg: TEvents[TEventName]) {
-    this._emitter.emit(eventName, ...(eventArg as []));
+    return this._emitter.emit(eventName, ...(eventArg as []));
   }
 
   on<TEventName extends keyof TEvents & string>(
@@ -42,6 +42,15 @@ export class EventEmitter<TEvents extends Record<string, any>> {
     handler: (...eventArg: TEvents[TEventName]) => void
   ) {
     this._emitter.on(eventName, handler as any);
+    return this;
+  }
+
+  once<TEventName extends keyof TEvents & string>(
+    eventName: TEventName,
+    handler: (...eventArg: TEvents[TEventName]) => void
+  ) {
+    this._emitter.once(eventName, handler as any);
+    return this;
   }
 
   off<TEventName extends keyof TEvents & string>(
@@ -49,6 +58,72 @@ export class EventEmitter<TEvents extends Record<string, any>> {
     handler: (...eventArg: TEvents[TEventName]) => void
   ) {
     this._emitter.off(eventName, handler as any);
+    return this;
+  }
+
+  addListener<TEventName extends keyof TEvents & string>(
+    eventName: TEventName,
+    listener: (...args: TEvents[TEventName]) => void
+  ) {
+    this._emitter.addListener(eventName, listener);
+    return this;
+  }
+
+  removeListener<TEventName extends keyof TEvents & string>(
+    eventName: TEventName,
+    listener: (...args: TEvents[TEventName]) => void
+  ) {
+    this._emitter.removeListener(eventName, listener);
+    return this;
+  }
+
+  removeAllListeners<TEventName extends keyof TEvents & string>(eventName?: TEventName | undefined) {
+    this._emitter.removeAllListeners(eventName);
+    return this;
+  }
+
+  setMaxListeners(n: number) {
+    this._emitter.setMaxListeners(n);
+    return this;
+  }
+
+  getMaxListeners(): number {
+    return this._emitter.getMaxListeners();
+  }
+
+  listeners<TEventName extends keyof TEvents & string>(eventName: TEventName) {
+    return this._emitter.listeners(eventName);
+  }
+
+  rawListeners<TEventName extends keyof TEvents & string>(eventName: TEventName) {
+    return this._emitter.rawListeners(eventName);
+  }
+
+  listenerCount<TEventName extends keyof TEvents & string>(
+    eventName: TEventName,
+    listener?: (...args: TEvents[TEventName]) => void
+  ) {
+    return this._emitter.listenerCount(eventName, listener);
+  }
+
+  prependListener<TEventName extends keyof TEvents & string>(
+    eventName: TEventName,
+    listener: (...args: TEvents[TEventName]) => void
+  ) {
+    this._emitter.prependListener(eventName, listener);
+    return this;
+  }
+
+  prependOnceListener<TEventName extends keyof TEvents & string>(
+    eventName: TEventName,
+    listener: (...args: TEvents[TEventName]) => void
+  ) {
+    this._emitter.prependOnceListener(eventName, listener);
+    return this;
+  }
+
+  eventNames() {
+    return this._emitter.eventNames();
   }
 }
 /* eslint-enable @typescript-eslint/no-explicit-any */
